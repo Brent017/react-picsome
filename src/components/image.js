e879f67aa580a13a7a4f443248192478a7dacea1
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-02-29 12:40:54 -0700

    images mapped

--- src/components/image.js
+++ src/components/image.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Image({ className }) {
+function Image({ className, img }) {
   return (
-    <div className={className}>
-      <img className="image-grid" />
+    <div className={`${className} image-container`}>
+      <img src={img.url} className="image-grid" />
     </div>
   );
 }
+
+export default Image;
